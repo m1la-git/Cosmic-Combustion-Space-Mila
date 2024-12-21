e51473496104ecfaedb3e8db85e29367f8be3945
kbekk <kbekkozhina@gmail.com>
2024-12-21 19:58:01 +0100

    abstract superclass MobileObject for future enemies and the Player

---
+++ core/src/de/tum/cit/ase/bomberquest/map/MobileObject.java
@@ -0,0 +1,94 @@
+package de.tum.cit.ase.bomberquest.map;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.physics.box2d.*;
+import de.tum.cit.ase.bomberquest.texture.Drawable;
+
+public abstract class MobileObject implements Drawable {
+    /**
+     * The Box2D hitbox of the player, used for position and collision detection.
+     */
+    private final Body hitbox;
+    private int speed;
+    /**
+     * Total time elapsed since the game started. We use this for calculating the player movement and animating it.
+     */
+    private float elapsedTime;
+
+    public MobileObject(World world, float x, float y, int speed) {
+        this.hitbox = createHitbox(world, x, y);
+        this.speed = speed;
+    }
+
+    /**
+     * Creates a Box2D body for the player.
+     * This is what the physics engine uses to move the player around and detect collisions with other bodies.
+     * @param world  The Box2D world to add the body to.
+     * @param startX The initial X position.
+     * @param startY The initial Y position.
+     * @return The created body.
+     */
+    private Body createHitbox(World world, float startX, float startY) {
+        // BodyDef is like a blueprint for the movement properties of the body.
+        BodyDef bodyDef = new BodyDef();
+        // Dynamic bodies are affected by forces and collisions.
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+        // Set the initial position of the body.
+        bodyDef.position.set(startX, startY);
+        // Create the body in the world using the body definition.
+        Body body = world.createBody(bodyDef);
+        // Now we need to give the body a shape so the physics engine knows how to collide with it.
+        // We'll use a circle shape for the player.
+        CircleShape circle = new CircleShape();
+        // Give the circle a radius of 0.3 tiles (the player is 0.6 tiles wide).
+        circle.setRadius(0.3f);
+        // Attach the shape to the body as a fixture.
+        // Bodies can have multiple fixtures, but we only need one for the player.
+        Fixture fixture = body.createFixture(circle, 1.0f);
+        fixture.setFilterData(new Filter());
+        // We're done with the shape, so we should dispose of it to free up memory.
+        circle.dispose();
+        // Set the player as the user data of the body, so we can look up the player from the body later.
+        body.setUserData(this);
+        return body;
+    }
+
+    public abstract void tick(float frameTime);
+
+    public float getElapsedTime() {
+        return elapsedTime;
+    }
+    public Body getHitbox() {
+        return hitbox;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void speedPowerUp() {
+        speed += 1;
+    }
+    public void setElapsedTime(float elapsedTime) {
+        this.elapsedTime = elapsedTime;
+    }
+    public int getCellX() {
+        return Math.round(getX());
+    }
+    public int getCellY() {
+        return Math.round(getY());
+    }
+    @Override
+    public abstract TextureRegion getCurrentAppearance();
+
+    @Override
+    public float getX() {
+        // The x-coordinate of the player is the x-coordinate of the hitbox (this can change every frame).
+        return hitbox.getPosition().x;
+    }
+
+    @Override
+    public float getY() {
+        // The y-coordinate of the player is the y-coordinate of the hitbox (this can change every frame).
+        return hitbox.getPosition().y;
+    }
+
+}
--- core/src/de/tum/cit/ase/bomberquest/map/Player.java
+++ core/src/de/tum/cit/ase/bomberquest/map/Player.java
@@ -16,58 +16,17 @@ import java.util.Deque;
  * Represents the player character in the game.
  * The player has a hitbox, so it can collide with other objects in the game.
  */
-public class Player implements Drawable {
-
-    /**
-     * The Box2D hitbox of the player, used for position and collision detection.
-     */
-    private final Body hitbox;
+public class Player extends MobileObject implements Drawable {
     /**
      * double queue to manage character movement
      */
     private final Deque<Integer> keyPressOrder = new ArrayDeque<>();
-    /**
-     * Total time elapsed since the game started. We use this for calculating the player movement and animating it.
-     */
-    private float elapsedTime;
+
 
     public Player(World world, float x, float y) {
-        this.hitbox = createHitbox(world, x, y);
+        super(world, x, y, 2);
     }
 
-    /**
-     * Creates a Box2D body for the player.
-     * This is what the physics engine uses to move the player around and detect collisions with other bodies.
-     *
-     * @param world  The Box2D world to add the body to.
-     * @param startX The initial X position.
-     * @param startY The initial Y position.
-     * @return The created body.
-     */
-    private Body createHitbox(World world, float startX, float startY) {
-        // BodyDef is like a blueprint for the movement properties of the body.
-        BodyDef bodyDef = new BodyDef();
-        // Dynamic bodies are affected by forces and collisions.
-        bodyDef.type = BodyDef.BodyType.DynamicBody;
-        // Set the initial position of the body.
-        bodyDef.position.set(startX, startY);
-        // Create the body in the world using the body definition.
-        Body body = world.createBody(bodyDef);
-        // Now we need to give the body a shape so the physics engine knows how to collide with it.
-        // We'll use a circle shape for the player.
-        CircleShape circle = new CircleShape();
-        // Give the circle a radius of 0.3 tiles (the player is 0.6 tiles wide).
-        circle.setRadius(0.3f);
-        // Attach the shape to the body as a fixture.
-        // Bodies can have multiple fixtures, but we only need one for the player.
-        Fixture fixture = body.createFixture(circle, 1.0f);
-        fixture.setFilterData(new Filter());
-        // We're done with the shape, so we should dispose of it to free up memory.
-        circle.dispose();
-        // Set the player as the user data of the body, so we can look up the player from the body later.
-        body.setUserData(this);
-        return body;
-    }
 
     /**
      * Move the player around in a circle by updating the linear velocity of its hitbox every frame.
@@ -75,8 +34,9 @@ public class Player implements Drawable {
      *
      * @param frameTime the time since the last frame.
      */
+    @Override
     public void tick(float frameTime) {
-        this.elapsedTime += frameTime;
+        setElapsedTime(getElapsedTime() + frameTime);
         // Make the player move in a circle with radius 2 tiles
         // You can change this to make the player move differently, e.g. in response to user input.
         // See Gdx.input.isKeyPressed() for keyboard input
@@ -84,20 +44,20 @@ public class Player implements Drawable {
         if (!keyPressOrder.isEmpty()) {
             switch (keyPressOrder.peekLast()) {
                 case Input.Keys.W, Input.Keys.UP:
-                    this.hitbox.setLinearVelocity(0, 2);
+                    getHitbox().setLinearVelocity(0, getSpeed());
                     break;
                 case Input.Keys.S, Input.Keys.DOWN:
-                    this.hitbox.setLinearVelocity(0, -2);
+                    getHitbox().setLinearVelocity(0, -(getSpeed()));
                     break;
                 case Input.Keys.A, Input.Keys.LEFT:
-                    this.hitbox.setLinearVelocity(-2, 0);
+                    getHitbox().setLinearVelocity(-(getSpeed()), 0);
                     break;
                 case Input.Keys.D, Input.Keys.RIGHT:
-                    this.hitbox.setLinearVelocity(2, 0);
+                    getHitbox().setLinearVelocity(getSpeed(), 0);
                     break;
             }
         } else {
-            this.hitbox.setLinearVelocity(0, 0);
+            getHitbox().setLinearVelocity(0, 0);
         }
     }
 
@@ -134,45 +94,23 @@ public class Player implements Drawable {
         });
     }
 
-    public Body getHitbox() {
-        return hitbox;
-    }
-
     @Override
     public TextureRegion getCurrentAppearance() {
         // Get the frame of the walk down animation that corresponds to the current time.
-
         if (!keyPressOrder.isEmpty()) {
             switch (keyPressOrder.peekLast()) {
                 case Input.Keys.W, Input.Keys.UP:
-                    return Animations.CHARACTER_WALK_UP.getKeyFrame(this.elapsedTime, true);
+                    return Animations.CHARACTER_WALK_UP.getKeyFrame(getElapsedTime(), true);
                 case Input.Keys.S, Input.Keys.DOWN:
-                    return Animations.CHARACTER_WALK_DOWN.getKeyFrame(this.elapsedTime, true);
+                    return Animations.CHARACTER_WALK_DOWN.getKeyFrame(getElapsedTime(), true);
                 case Input.Keys.A, Input.Keys.LEFT:
-                    return Animations.CHARACTER_WALK_LEFT.getKeyFrame(this.elapsedTime, true);
+                    return Animations.CHARACTER_WALK_LEFT.getKeyFrame(getElapsedTime(), true);
                 case Input.Keys.D, Input.Keys.RIGHT:
-                    return Animations.CHARACTER_WALK_RIGHT.getKeyFrame(this.elapsedTime, true);
+                    return Animations.CHARACTER_WALK_RIGHT.getKeyFrame(getElapsedTime(), true);
             }
         }
         return SpriteSheet.CHARACTER.at(1, 1);
     }
 
-    public int getCellX() {
-        return Math.round(getX());
-    }
-    public int getCellY() {
-        return Math.round(getY());
-    }
 
-    @Override
-    public float getX() {
-        // The x-coordinate of the player is the x-coordinate of the hitbox (this can change every frame).
-        return hitbox.getPosition().x;
-    }
-
-    @Override
-    public float getY() {
-        // The y-coordinate of the player is the y-coordinate of the hitbox (this can change every frame).
-        return hitbox.getPosition().y;
-    }
 }
